85ff32928e1353394f2bac6dda6cc7c4ecc1b74d
maktony1 <ghkdehdgnl12@naver.com>
2020-07-30 12:32:58 +0900

    aop, LogAop

---
+++ src/main/java/edu/bit/board/aop/LogAOP.java
@@ -0,0 +1,47 @@
+package edu.bit.board.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LogAOP {
+
+//  @Pointcut("execution(public void get*(..))")   // public void인 모든 get메소드
+//  @Pointcut("execution(* com.javalec.ex.*.*())")   // com.javalec.ex 패키지에 파라미터가 없는 모든 메소드
+//  @Pointcut("execution(* com.javalec.ex..*.*())")   // com.javalec.ex 패키지 & com.javalec.ex 하위 패키지에 파라미터가 없는 모든 메소드
+//  @Pointcut("execution(* com.javalec.ex.Worker.*())")   // com.javalec.ex.Worker 안의 모든 메소드
+
+//  @Pointcut("within(com.javalec.ex.*)")   //com.javalec.ex 패키지 안에 있는 모든 메소드
+//  @Pointcut("within(com.javalec.ex..*)")  //com.javalec.ex 패키지 및 하위 패키지 안에 있는 모든 메소드
+//  @Pointcut("within(com.javalec.ex.Worker)") //com.javalec.ex.Worker 모든 메소드
+  
+//  @Pointcut("bean(student)")   //student 빈에만 적용
+//  @Pointcut("bean(*ker)")      //~ker로 끝나는 빈에만 적용
+	
+	public Object loggerAop(ProceedingJoinPoint joinpoint) throws Throwable {
+		String signatureStr = joinpoint.getSignature().toShortString();
+		System.out.println( signatureStr + " is start.");
+
+		long st = System.currentTimeMillis();
+		try {
+			Object obj = joinpoint.proceed();
+			
+			return obj;
+		}finally {
+			
+			long et = System.currentTimeMillis();
+			
+			System.out.println( signatureStr + " is finished.");
+			System.out.println( signatureStr + " 경과시간 : " + (et - st));
+		} 
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- src/main/java/edu/bit/board/aop/LogAdvice.java
+++ src/main/java/edu/bit/board/aop/LogAdvice.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LogAdvice {
 	
+	
 	@Before("within(edu.bit.board.service.*)")
 	public void printLogging() {  
 		System.out.println("공통기능 - 프린트 로그");
